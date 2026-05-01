92290638632acc61aa55a534864081d81581af4e
benanciaux <benanciaux@icloud.com>
2026-05-01 15:24:31 -0500

    Update echo.py

--- echo.py
+++ echo.py
@@ -5,12 +5,11 @@ robot = RobotInterface()
 def on_speech(text):
     print("Heard:", text)
     
-    # Send it back to speaker
+    #send it back to speaker
     robot.audio.tts(text)
 
-# Hook into robot's speech system
 robot.audio.set_speech_callback(on_speech)
 
-# Keep running
+#keep running
 while True:
     pass
